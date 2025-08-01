56d45a8f1705868d1133eac2f1642ddd3dc80442
Sydny Jane R. Quinquino <sjquinquino71936@liceo.edu.ph>
2025-08-01 11:13:18 +0800

    lab activity 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main Branch!");
+        Scanner scanner = new Scanner(System.in);
+        int totalNumber = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter number " + i + ": ");
+            int num = scanner.nextInt();
+            totalNumber += num;
+        }
+
+        System.out.println("Total Number: " + totalNumber);
+
+        scanner.close();
     }
 }
