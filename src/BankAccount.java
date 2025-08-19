e34fd460395a6bf2579a5d409d81f0fdf1cbe1df
Sydny Jane R. Quinquino <sjquinquino71936@liceo.edu.ph>
2025-08-19 11:49:34 +0800

    lab activity 5

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
-
 public class BankAccount {
+    static String bankName = "Liceo Bank";
+    static int totalAccounts = 0;
+    static double interestRate = 0.03;
+    static int accountCounter = 1;
+
+    String accountNumber;
+    String accountHolderName;
+    double balance;
+
+    public BankAccount(String name, double initialBalance) {
+        accountNumber = "ACC" + String.format("%03d", accountCounter++);
+        accountHolderName = name;
+        balance = initialBalance;
+        totalAccounts++;
+        System.out.println("Account Created: " + accountNumber + " for " + name + " with initial balance: $" + initialBalance);
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println(accountHolderName + " deposited $" + amount + ". New balance: $" + balance);
+    }
+
+    public void withdraw(double amount) {
+        if (amount > balance) {
+            System.out.println("Insufficient balance for " + accountHolderName);
+        } else {
+            balance -= amount;
+            System.out.println(accountHolderName + " withdrew $" + amount + ". New balance: $" + balance);
+        }
+    }
+
+    public double calculateInterest() {
+        return balance * interestRate;
+    }
 }
